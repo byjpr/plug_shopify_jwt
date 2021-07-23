a1cc1eb9f9ac037bf33dd41d2427e5345c2fc6cb
Jordan Parker <jgit@fmail.co.uk>
2021-07-23 17:23:00 +0000

    📝 Adds more detailed moduledocs

--- lib/plug_shopify_jwt.ex
+++ lib/plug_shopify_jwt.ex
@@ -1,10 +1,75 @@
 defmodule PlugShopifyEmbeddedJWTAuth do
   @moduledoc """
-  Validate Shopify JWT
+    This plug validates Shopify JWT - also known as session token authentication.
+    Session tokens/JWT are a replacement for cookie based authentication in embedded apps.
+
+    PlugShopifyJwt is architected to support Session tokens whilst allowing you to
+    verify with URL parameters (validation of URL parameters is not included in this plug) should you decide.
+
+    ### Usage
+    Grab you app secret, and crack open your router.ex file, insert
+    `plug PlugShopifyEmbeddedJWTAuth, [secret: "your-secret"]`. A basic setup looks something similar to this:
+
+    ```elixir
+      pipeline :embedded do
+        plug PlugShopifyEmbeddedJWTAuth, [secret: "224e5146-4f1e-4a1d-a64a-2732df659542"]
+      end
+
+      scope "/api", HelloPhoenixWeb do
+        pipe_through :embedded
+
+        get "/show", PageController, :show
+      end
+    ```
+
+    ### Installation
+    The package can be installed by adding `plug_shopify_jwt` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [
+        {:plug_shopify_jwt, "~> 0.1.0"}
+      ]
+    end
+    ```
   """
   import Plug.Conn
 
-  @spec init(keyword) :: keyword
+  @typedoc """
+  ### Plug configuration
+
+  - algorithm (Default: `"HS256"`) - "HS256", "HS384", "HS512", "RS256", "RS384", "RS512", "ES256", "ES384", "ES512", "PS256", "PS384", "PS512", "Ed25519", "Ed25519ph", "Ed448".
+  - secret (**Required** the plug will throw an error if you do not set this) - from your Shopify Partner Dashboard.
+  - halt_on_error (Default: `true`) - either `true` or `false`
+  - signer (App only) - this is set by the Plug, you do not need to set this item.
+  """
+  @type config :: [
+          algorithm: String.t(),
+          secret: String.t() | nil,
+          signer: Joken.Signer.t() | nil,
+          halt_on_error: true | false
+        ]
+
+  @doc """
+  Protects a route with JWT. Plug will automatically call `call/2` for each connection.
+
+  ### Config
+  See the documentation for the `config` type for details on the available options.
+
+  ### Usage:
+    ```elixir
+      pipeline :embedded do
+        plug PlugShopifyEmbeddedJWTAuth, [secret: "224e5146-4f1e-4a1d-a64a-2732df659542"]
+      end
+
+      scope "/api", HelloPhoenixWeb do
+        pipe_through :embedded
+
+        get "/show", PageController, :show
+      end
+    ```
+  """
+  @spec init(config) :: config
   def init(opts) do
     opts
     |> prepare_cfg(Application.get_all_env(:plug_shopify_jwt))
@@ -12,6 +77,14 @@ defmodule PlugShopifyEmbeddedJWTAuth do
     |> set_signer()
   end
 
+  @doc """
+  Pass in a `Plug.Conn` and `Config` and it will take the auth header, authenticate the request, and the result will be one of the following:
+
+  1. *Return a 401*: `halt_on_error` must be set to `true`, and the authentication fails.
+  2. *Allow the connection to continue*: When authentication passes we will set `conn.private[:shopify_jwt_claims]` to the full decoded JWT, and `conn.private[:current_shop_name]` - will be the myshopify.com domain for the current store, e.g. `example.shopify.com`
+  3. *Fail, but allow the connection to continue*: When `halt_on_error` is set to `false` instead of halting the connection the plug will set `conn.private[:ps_jwt_success]` to `false` assuming you will be doing error handling in another place.
+  """
+  @spec call(Plug.Conn.t(), config) :: Plug.Conn.t()
   def call(conn = %{private: %{shop_origin_type: :jwt}}, opts) do
     conn
     |> get_auth_header
